55e7c1648b315b98a1387c41738f946393e1834e
Kyung-A <nek1717@naver.com>
2023-12-04 17:40:31 +0900

    feat: 네이버 맵 렌더링

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useRef } from "react";
 import "../styles/globals.css";
 
+type NaverMap = naver.maps.Map;
+
 export default function Home() {
+  const mapRef = useRef<NaverMap | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      const mapOptions = {
+        center: new naver.maps.LatLng(37.3595704, 127.105399),
+        zoom: 10,
+      };
+
+      const map = new naver.maps.Map("map", mapOptions);
+      mapRef.current = map;
+    }
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main>
+      <div id="map" style={{ width: "100vw", height: "100vh" }}></div>
+    </main>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
- 
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
 export default class MyDocument extends Document {
   render() {
     return (
@@ -8,8 +9,12 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Script
+            strategy="beforeInteractive"
+            src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NAVER_CLIENT_ID}`}
+          />
         </body>
       </Html>
-    )
+    );
   }
 }
