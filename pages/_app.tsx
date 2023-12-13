c96f038651ae5ee0ab6f44d034b6d8e4e6481eff
Nam Eungyeong <nek1717@naver.com>
2023-12-13 22:48:15 +0900

    feat: 필터, 페이지네이션 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,33 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import Image from "next/image";
+
 import Search from "../components/Search";
 import { categories } from "../mock/categories";
-
 import Category from "../components/Category";
-
-import "../styles/globals.css";
 import Card from "../components/Card";
 import { IMarkerResp, requestMarkerList } from "../shared/api";
+import { filters } from "../mock/filters";
+import { useActive } from "../shared/hooks/useActive";
+
+import "../styles/globals.css";
 
 type NaverMap = naver.maps.Map;
 
 export default function Home() {
   const mapRef = useRef<NaverMap | null>(null);
   const [data, setData] = useState<IMarkerResp[]>();
+
+  const { changeActive: changeFilterActive, result: filterList } = useActive(
+    filters,
+    true
+  );
+  const { changeActive: changeCategoryActive, result: categoryList } =
+    useActive(categories);
+
+  const isAllFilter = useMemo(
+    () => filterList.find((filter) => filter.isActive)?.id,
+    [filterList]
+  );
 
   const fetchMarkerList = useCallback(async () => {
     const data = await requestMarkerList();
@@ -43,13 +57,19 @@ export default function Home() {
       <header className="absolute top-0 left-0 z-10 mt-6 ml-6">
         <div className="flex items-center">
           <Search />
-          <div className="inline-flex ml-2 px-6 bg-white h-12 font-semibold rounded-full items-center box-border text-lg shadow-[0px_0px_15px_0px_rgba(0,0,0,0.20)]">
+          <div className="inline-flex ml-2 px-6 bg-white h-12 font-semibold rounded-full items-center box-border shadow-[0px_0px_15px_0px_rgba(0,0,0,0.20)]">
             대흥동
           </div>
         </div>
         <div className="flex items-center gap-1 mt-3">
-          {categories.map((category) => (
-            <Category key={category.id} category={category} />
+          {categoryList.map((category) => (
+            <Category
+              onClick={() => {
+                changeCategoryActive(category.id as string);
+              }}
+              key={category.id as string}
+              category={category}
+            />
           ))}
         </div>
       </header>
@@ -67,14 +87,49 @@ export default function Home() {
             />
           </button>
           <aside className="bg-white h-screen w-[420px] grid shadow-[-5px_0px_10px_0px_rgba(0,0,0,0.05)] overflow-hidden">
-            <div className="pt-11 px-8 pb-4">
-              <h1 className="text-darkMain text-2xl leading-tight font-semibold">
+            <div className="pt-6 px-8 pb-4">
+              <div className="flex items-center justify-between min-h-[25px]">
+                <div className="flex items-center gap-1">
+                  {filterList.map((filter) => (
+                    <button
+                      onClick={() => changeFilterActive(filter.id as string)}
+                      key={filter.id as string}
+                      className={`text-neutral-400 bg-neutral-200 border text-xs font-medium rounded-full w-[80px] py-[2px] ${
+                        filter.isActive
+                          ? "border-neutral-400"
+                          : "border-transparent"
+                      }`}
+                    >
+                      {filter.name}
+                    </button>
+                  ))}
+                </div>
+                {isAllFilter === "all" && (
+                  <div className="flex items-center gap-3">
+                    <button className="text-sm text-neutral-400 hover:text-neutral-800">
+                      &#60;
+                    </button>
+                    {[1, 2, 3, 4, 5].map((num, index) => (
+                      <button
+                        key={index}
+                        className="text-sm text-neutral-400 hover:text-neutral-800"
+                      >
+                        {num}
+                      </button>
+                    ))}
+                    <button className="text-sm text-neutral-400 hover:text-neutral-800">
+                      &#62;
+                    </button>
+                  </div>
+                )}
+              </div>
+              <h1 className="text-darkMain mt-3 text-2xl leading-tight font-semibold">
                 현재 대흥동 지역에서
                 <br />
                 가장 인기 있는 음식점이에요!
               </h1>
             </div>
-            <div className="overflow-y-auto px-8 pt-4 pb-8">
+            <div className="overflow-y-auto px-8 pt-6 pb-8">
               {data?.map((item) => (
                 <div key={item.CafeId} className="mb-5 last:mb-0">
                   <Card data={item} />
