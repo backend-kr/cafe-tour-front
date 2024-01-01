caa099db9fe9be12827604de7642d94c6ceba4b9
Nam Eungyeong <nek1717@naver.com>
2024-01-01 19:30:52 +0900

    feat: 코드 분리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,4 @@
-import {
-  useContext,
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
-import Image from "next/image";
+import { useContext, useCallback, useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/router";
 
 import Card from "../components/Card";
@@ -15,7 +7,6 @@ import { filters } from "../mock/filters";
 import { useActive } from "../shared/hooks/useActive";
 import { useAuth } from "../shared/contexts/Auth";
 import { useMyTourToggle } from "../shared/hooks/useMyTourToggle";
-import MapLayout from "../shared/layout/MapLayout";
 import { CategoryContext } from "../shared/contexts/Category";
 import { useMap } from "../shared/contexts/Map";
 
@@ -45,33 +36,36 @@ export default function Home() {
   );
 
   const fetchMarkerList = useCallback(async () => {
-    const data = await requestMarkerList(isSign);
-    let markerList: naver.maps.Marker[] = [];
+    if (mapRef.map) {
+      const data = await requestMarkerList(isSign);
+      let markerList: naver.maps.Marker[] = [];
 
-    data.forEach((mark) => {
-      markerList.push(
-        new naver.maps.Marker({
-          position: new naver.maps.LatLng(
-            Number(mark.latitude),
-            Number(mark.longitude)
-          ),
-          // map: mapRef.current as NaverMap,
-        })
+      data.forEach((mark) => {
+        markerList.push(
+          new naver.maps.Marker({
+            position: new naver.maps.LatLng(
+              Number(mark.latitude),
+              Number(mark.longitude)
+            ),
+            map: mapRef.map!.current as NaverMap,
+          })
+        );
+      });
+
+      const mapCenter = new naver.maps.LatLng(
+        Number(data[0]?.latitude),
+        Number(data[0]?.longitude)
       );
-    });
+      mapRef.map!.current!.setCenter(mapCenter);
 
-    const mapCenter = new naver.maps.LatLng(
-      Number(data[0]?.latitude),
-      Number(data[0]?.longitude)
-    );
-    // (mapRef.current as NaverMap).setCenter(mapCenter);
-
-    setMarker(markerList);
-    setData(data);
-  }, [isSign]);
+      setMarker(markerList);
+      setData(data);
+    }
+  }, [isSign, mapRef]);
 
   const resetMap = useCallback(() => {
     setData(null);
+    setMarker(null);
     marker?.forEach((v) => {
       v.setMap(null);
     });
@@ -95,89 +89,63 @@ export default function Home() {
     }
   }, [isCategoryActive]);
 
-  useEffect(() => {
-    console.log(mapRef.map);
-  }, [mapRef]);
-
   return (
     <>
-      <div className="absolute top-0 right-0">
-        <div className="flex">
-          {isSign && (
-            <button
-              onClick={() =>
-                void router.push("/myTour", undefined, { shallow: true })
-              }
-              className="bg-main mt-6 mr-6 w-16 h-16 rounded-full z-20 flex items-center justify-center shadow-[0px_0px_10px_0px_rgba(0,0,0,0.25)]"
-            >
-              <Image
-                src="svg/user_pin.svg"
-                width={36}
-                height={36}
-                alt="나의 즐겨찾기"
-              />
-            </button>
-          )}
-          {isCategoryActive && (
-            <aside className="bg-white h-screen w-[420px] grid shadow-[-5px_0px_10px_0px_rgba(0,0,0,0.05)] overflow-hidden">
-              <div className="pt-6 px-8 pb-4">
-                <div className="flex items-center justify-between min-h-[25px]">
-                  <div className="flex items-center gap-1">
-                    {filterList.map((filter) => (
-                      <button
-                        onClick={() => changeFilterActive(filter.id as string)}
-                        key={filter.id as string}
-                        className={`text-neutral-400 bg-neutral-200 border text-xs font-medium rounded-full w-[80px] py-[2px] ${
-                          filter.isActive
-                            ? "border-neutral-400"
-                            : "border-transparent"
-                        }`}
-                      >
-                        {filter.name}
-                      </button>
-                    ))}
-                  </div>
-                  {isAllFilter && (
-                    <div className="flex items-center gap-3">
-                      <button className="text-sm text-neutral-400 hover:text-neutral-800">
-                        &#60;
-                      </button>
-                      {[1, 2, 3, 4, 5].map((num, index) => (
-                        <button
-                          key={index}
-                          className="text-sm text-neutral-400 hover:text-neutral-800"
-                        >
-                          {num}
-                        </button>
-                      ))}
-                      <button className="text-sm text-neutral-400 hover:text-neutral-800">
-                        &#62;
-                      </button>
-                    </div>
-                  )}
-                </div>
-                {!isAllFilter && (
-                  <h1 className="text-darkMain mt-3 text-2xl leading-tight font-semibold">
-                    현재 대흥동 지역에서
-                    <br />
-                    가장 인기 있는 음식점이에요!
-                  </h1>
-                )}
-              </div>
-              <div className="overflow-y-auto px-8 pt-6 pb-8">
-                {data?.map((item) => (
-                  <div key={item.CafeId} className="mb-5 last:mb-0">
-                    <Card
-                      data={item}
-                      onClick={() => onClickSave(item.CafeId)}
-                    />
-                  </div>
+      {isCategoryActive && (
+        <aside className="bg-white h-screen w-[420px] grid shadow-[-5px_0px_10px_0px_rgba(0,0,0,0.05)] overflow-hidden">
+          <div className="pt-6 px-8 pb-4">
+            <div className="flex items-center justify-between min-h-[25px]">
+              <div className="flex items-center gap-1">
+                {filterList.map((filter) => (
+                  <button
+                    onClick={() => changeFilterActive(filter.id as string)}
+                    key={filter.id as string}
+                    className={`text-neutral-400 bg-neutral-200 border text-xs font-medium rounded-full w-[80px] py-[2px] ${
+                      filter.isActive
+                        ? "border-neutral-400"
+                        : "border-transparent"
+                    }`}
+                  >
+                    {filter.name}
+                  </button>
                 ))}
               </div>
-            </aside>
-          )}
-        </div>
-      </div>
+              {isAllFilter && (
+                <div className="flex items-center gap-3">
+                  <button className="text-sm text-neutral-400 hover:text-neutral-800">
+                    &#60;
+                  </button>
+                  {[1, 2, 3, 4, 5].map((num, index) => (
+                    <button
+                      key={index}
+                      className="text-sm text-neutral-400 hover:text-neutral-800"
+                    >
+                      {num}
+                    </button>
+                  ))}
+                  <button className="text-sm text-neutral-400 hover:text-neutral-800">
+                    &#62;
+                  </button>
+                </div>
+              )}
+            </div>
+            {!isAllFilter && (
+              <h1 className="text-darkMain mt-3 text-2xl leading-tight font-semibold">
+                현재 대흥동 지역에서
+                <br />
+                가장 인기 있는 음식점이에요!
+              </h1>
+            )}
+          </div>
+          <div className="overflow-y-auto px-8 pt-6 pb-8">
+            {data?.map((item) => (
+              <div key={item.CafeId} className="mb-5 last:mb-0">
+                <Card data={item} onClick={() => onClickSave(item.CafeId)} />
+              </div>
+            ))}
+          </div>
+        </aside>
+      )}
     </>
   );
 }
--- shared/layout/MapLayout.tsx
+++ shared/layout/MapLayout.tsx
@@ -1,15 +1,20 @@
 import { ReactNode, useContext } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 import Category from "../../components/Category";
 import Search from "../../components/Search";
 import { CategoryContext } from "../contexts/Category";
+import { useAuth } from "../contexts/Auth";
 
 interface IMapLayout {
   children: ReactNode;
 }
 
 const MapLayout = ({ children }: IMapLayout) => {
+  const router = useRouter();
   const { result, setState } = useContext(CategoryContext);
+  const { isSign } = useAuth();
 
   return (
     <div className="relative">
@@ -35,7 +40,26 @@ const MapLayout = ({ children }: IMapLayout) => {
       <main className="z-10">
         <div id="map" className="w-screen h-screen"></div>
       </main>
-      {children}
+      <div className="absolute top-0 right-0">
+        <div className="flex">
+          {isSign && (
+            <button
+              onClick={() =>
+                void router.push("/myTour", undefined, { shallow: true })
+              }
+              className="bg-main mt-6 mr-6 w-16 h-16 rounded-full z-20 flex items-center justify-center shadow-[0px_0px_10px_0px_rgba(0,0,0,0.25)]"
+            >
+              <Image
+                src="svg/user_pin.svg"
+                width={36}
+                height={36}
+                alt="나의 즐겨찾기"
+              />
+            </button>
+          )}
+          {children}
+        </div>
+      </div>
     </div>
   );
 };
