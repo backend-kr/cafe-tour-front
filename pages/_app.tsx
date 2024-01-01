99d24305ea8fadea3be9606c58bafaf8d7c0a7f8
Nam Eungyeong <nek1717@naver.com>
2024-01-01 18:06:04 +0900

    feat: 코드 분리 및 Provider 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import type { AppProps } from "next/app";
+import AuthProvider from "../shared/contexts/Auth";
+import { CategoryContext } from "../shared/contexts/Category";
+import { useActive } from "../shared/hooks/useActive";
+import { categories } from "../mock/categories";
+import MapLayout from "../shared/layout/MapLayout";
+import MapProvider from "../shared/contexts/Map";
+
 import "../styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  const { changeActive: changeCategoryActive, result: categoryList } =
+    useActive(categories);
+
+  return (
+    <CategoryContext.Provider
+      value={{ setState: changeCategoryActive, result: categoryList }}
+    >
+      <AuthProvider>
+        <MapProvider>
+          <MapLayout>
+            <Component {...pageProps} />
+          </MapLayout>
+        </MapProvider>
+      </AuthProvider>
+    </CategoryContext.Provider>
+  );
 };
 
 export default App;
