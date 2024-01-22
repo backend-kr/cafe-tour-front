cb7ab7b3f21d2744b84bb980463cb6fe6759e5e6
Nam Eungyeong <nek1717@naver.com>
2024-01-22 16:44:39 +0900

    test: 환경변수 에러 배포 테스트

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const NAVER_KEY = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
-
 const nextConfig = {
   images: {
     disableStaticImages: true,
     domains: ["ldb-phinf.pstatic.net"],
     minimumCacheTTL: 31536000,
-  },
-  env: {
-    NAVER_CLIENT_ID: NAVER_KEY,
   },
 };
 
