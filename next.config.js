076167e762bef1a90ee78b6624408ace4cdfe6e5
Nam Eungyeong <nek1717@naver.com>
2024-01-22 15:55:38 +0900

    fix: env 환경변수 에러 수정

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const NAVER_KEY = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
+
 const nextConfig = {
   images: {
     disableStaticImages: true,
-    domains: ['ldb-phinf.pstatic.net'],
+    domains: ["ldb-phinf.pstatic.net"],
     minimumCacheTTL: 31536000,
   },
   env: {
-    NAVER_CLIENT_ID: "#NAVER_CLIENT_ID",
+    NAVER_CLIENT_ID: NAVER_KEY,
   },
 };
 
