549c51198aa78b40d0a040e82386390c80698d57
Nam Eungyeong <nek1717@naver.com>
2024-01-22 17:35:28 +0900

    test: script 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,17 +7,14 @@ export default class MyDocument extends Document {
       <Html>
         <Head />
         <body>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              var script = document.createElement('script');
-              script.src = 'https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}';
-              document.body.appendChild(script);
-            `,
-            }}
-          />
           <Main />
           <NextScript />
+          <Script
+            strategy="afterInteractive"
+            type="text/javascript"
+            src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}`}
+            onError={(e) => console.error("Error", e)}
+          />
         </body>
       </Html>
     );
