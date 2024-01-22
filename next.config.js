7c840955117732133cf0a7ec0617aa1e5e511690
Nam Eungyeong <nek1717@naver.com>
2024-01-22 16:08:42 +0900

    fix: 환경설정 되돌림

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const NAVER_KEY = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
-
 const nextConfig = {
   images: {
     disableStaticImages: true,
@@ -9,7 +6,7 @@ const nextConfig = {
     minimumCacheTTL: 31536000,
   },
   env: {
-    NAVER_CLIENT_ID: NAVER_KEY,
+    NAVER_CLIENT_ID: "#NAVER_CLIENT_ID",
   },
 };
 
