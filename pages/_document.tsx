651263829c9ae05e55f141317b632ba798158670
Nam Eungyeong <nek1717@naver.com>
2024-01-30 19:51:53 +0900

    fix: script load 변경

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,10 +10,8 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
           <Script
-            strategy="afterInteractive"
-            type="text/javascript"
+            strategy="beforeInteractive"
             src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}`}
-            onError={(e) => console.error("Error", e)}
           />
         </body>
       </Html>
